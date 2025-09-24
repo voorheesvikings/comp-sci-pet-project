7d008894bd178c2af87c5c7044e4e4860ee7aa00
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-09-24 12:08:56 +0000

    update

--- src/com/mypetproject/Pet.java
+++ src/com/mypetproject/Pet.java
@@ -9,6 +9,20 @@ public class Pet{
     }
 
     public void getSpecies() {
+        System.out.println("loading...");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("thinking...");
+        try {
+            Thread.sleep(3000); 
+        } catch (InterruptedException e) {
+            
+            Thread.currentThread().interrupt(); 
+        }
+
         System.out.println("This pet is a "  + species);
         return;
     }
