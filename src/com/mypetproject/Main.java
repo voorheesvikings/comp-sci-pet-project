16886c4520367928f2dc0f153c648e234dab945f
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-09-24 12:31:33 +0000

    update

--- src/com/mypetproject/Main.java
+++ src/com/mypetproject/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Pet myPet = new Pet("Cat");
         myPet.getSpecies();
+        Cat myCat = new Cat("Cat", 3, "Black");
+        myCat.getDetails();
     }
 }
